dfb396352146ff8bfe1cd0399070f71cd3ee6731
Anton Oresten <antonoresten@gmail.com>
2024-12-27 18:52:43 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,27 @@ using Auton: CodeBlock, codeblocks
 
 @testset "Auton.jl" begin
     
-    @testset "CodeBlock" begin
-        @test codeblocks("x = 1; sin(x)") == [CodeBlock("x = 1"), CodeBlock("sin(x)")]
+    @testset "code.jl" begin
+
+        @testset "CodeBlock" begin
+            @test CodeBlock("x = 1") isa CodeBlock{:julia}
+            @test CodeBlock("x = 1", nothing) isa CodeBlock{:julia}
+            @test CodeBlock("x = 1", "python") isa CodeBlock{:python}
+        end
+
+        @testset "codeblocks" begin
+            code = """
+            ```julia
+            x = 1
+            ```
+            ```python
+            print("Hello, world!")
+            ```
+            """
+            @test codeblocks(code) ==
+                CodeBlock[CodeBlock("x = 1"), CodeBlock("print(\"Hello, world!\")", "python")]
+        end
+
     end
 end
 
